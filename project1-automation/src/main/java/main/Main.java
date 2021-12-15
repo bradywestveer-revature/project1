f94e70e8c4fb8f282c579fd04fe8da82737b3b5a
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2021-12-15 12:47:00 -0600

    refactoring and bugfixing in automation project

--- project1-automation/src/main/java/main/Main.java
+++ project1-automation/src/main/java/main/Main.java
@@ -15,33 +15,47 @@ public class Main {
 		WebDriver driver = new ChromeDriver ();
 		WebDriverWait wait = new WebDriverWait (driver, Duration.ofSeconds (5));
 		
-		driver.get ("http://localhost/login");
+		try {
+			driver.get ("http://localhost/login");
+			
+			WebElement usernameInput = driver.findElement (By.id ("usernameInput"));
+			WebElement passwordInput = driver.findElement (By.id ("passwordInput"));
+			
+			WebElement loginButton = driver.findElement (By.id ("loginButton"));
+			
+			usernameInput.sendKeys ("employee1");
+			passwordInput.sendKeys ("password");
+			
+			loginButton.click ();
+			
+			By requestsSelector = By.className ("request");
+			
+			wait.until (ExpectedConditions.numberOfElementsToBeMoreThan (requestsSelector, 0));
+			
+			int numberOfRequests = driver.findElements (requestsSelector).size ();
+			
+			By requestsCreateAmountSelector = By.id ("requestsCreateAmount");
+			By requestsCreateTypeSelector = By.id ("requestsCreateType");
+			By requestsCreateDescriptionSelector = By.id ("requestsCreateDescription");
+			By requestsCreateSubmitButtonSelector = By.id ("requestsCreateSubmitButton");
+			
+			wait.until (ExpectedConditions.elementToBeClickable (requestsCreateAmountSelector));
+			wait.until (ExpectedConditions.elementToBeClickable (requestsCreateTypeSelector));
+			wait.until (ExpectedConditions.elementToBeClickable (requestsCreateDescriptionSelector));
+			wait.until (ExpectedConditions.elementToBeClickable (requestsCreateSubmitButtonSelector));
+			
+			driver.findElement (requestsCreateAmountSelector).sendKeys ("101.01");
+			new Select (driver.findElement (requestsCreateTypeSelector)).selectByIndex (3);
+			driver.findElement (requestsCreateDescriptionSelector).sendKeys ("AUTOMATED DESCRIPTION");
+			
+			driver.findElement (requestsCreateSubmitButtonSelector).click ();
+			
+			wait.until (ExpectedConditions.numberOfElementsToBeMoreThan (requestsSelector, numberOfRequests));
+		}
 		
-		WebElement usernameInput = driver.findElement (By.id ("usernameInput"));
-		WebElement passwordInput = driver.findElement (By.id ("passwordInput"));
-		
-		WebElement loginButton = driver.findElement (By.id ("loginButton"));
-		
-		usernameInput.sendKeys ("employee1");
-		passwordInput.sendKeys ("password");
-		
-		loginButton.click ();
-		
-		By requestsCreateAmountSelector = By.id ("requestsCreateAmount");
-		By requestsCreateTypeSelector = By.id ("requestsCreateType");
-		By requestsCreateDescriptionSelector = By.id ("requestsCreateDescription");
-		By requestsCreateSubmitButtonSelector = By.id ("requestsCreateSubmitButton");
-		
-		wait.until (ExpectedConditions.elementToBeClickable (requestsCreateAmountSelector));
-		wait.until (ExpectedConditions.elementToBeClickable (requestsCreateTypeSelector));
-		wait.until (ExpectedConditions.elementToBeClickable (requestsCreateDescriptionSelector));
-		wait.until (ExpectedConditions.elementToBeClickable (requestsCreateSubmitButtonSelector));
-		
-		driver.findElement (requestsCreateAmountSelector).sendKeys ("101.01");
-		new Select (driver.findElement (requestsCreateTypeSelector)).selectByIndex (3);
-		driver.findElement (requestsCreateDescriptionSelector).sendKeys ("AUTOMATED DESCRIPTION");
-		
-		driver.findElement (requestsCreateSubmitButtonSelector).click ();
+		catch (Exception exception) {
+			exception.printStackTrace ();
+		}
 		
 		driver.quit ();
 	}
